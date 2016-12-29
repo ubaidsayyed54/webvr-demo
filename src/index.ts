ae5981d5251a052a7cda287ef0a8b00a6291f222
Yiu Man Chi <ymcatar@gmail.com>
2016-12-29 11:51:16 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as Three from 'three';
+import 'webvr-polyfill';
 
 declare function require(string): string;
 declare function VRFrameData(): void;
@@ -8,111 +9,154 @@ declare function VRFrameData(): void;
 const NEAR = 0.1;
 const FAR = 10000;
 
-let scene = new Three.Scene();
-let camera = new Three.PerspectiveCamera(45, window.innerWidth / window.innerHeight, NEAR, FAR);
-let renderer = new Three.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xffffff, 1);
-document.body.appendChild(renderer.domElement);
+interface ObjectOf<T> {
+  [key: string]: T;
+}
 
-/* prepare light */
+class App {
 
-let ambientLight = new Three.AmbientLight(0xffffff, 0.5);
-let light = new Three.DirectionalLight(0xffffff, 1);
-light.position.set(10, 20, 20);
-scene.add(ambientLight);
-scene.add(light);
+  private scene: Three.Scene;
+  private camera: Three.PerspectiveCamera;
+  private renderer: Three.WebGLRenderer;
+  private lights: ObjectOf<Three.Light>;
+  private objects: ObjectOf<Three.Object3D>;
 
-/* prepare geometry */
+  private firstVRFrame:boolean = true;
+  private vrDisplay:any;
 
-let loader = new Three.TextureLoader();
-
-let torus:Three.Object3D;
-loader.load(require('./textures/earth.jpg'), map => {
-  loader.load(require('./textures/earth_normal.jpg'), normalMap => {
-    let geometry = new Three.SphereGeometry(2, 256, 256, 256);
-    let material = new Three.MeshPhongMaterial({
-      map,
-      normalMap,
-      shininess: 0
-    });
-    torus = new Three.Mesh(geometry, material);
-    torus.position.z = -10;
-    scene.add(torus);
-  });
-});
-
-/* initialize everything else */
-
-this.firstVRFrame = true;
-
-let render = () => {
-  // for first frame, use the standard api
-  if (this.firstVRFrame) {
-    this.firstVRFrame = false;
-    return this.vrDisplay.requestAnimationFrame(render);
+  constructor() {
+    this.scene = new Three.Scene();
+    this.camera = new Three.PerspectiveCamera(45, window.innerWidth / window.innerHeight, NEAR, FAR);
+    this.renderer = new Three.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setClearColor(0xffffff, 1);
+    document.body.appendChild(this.renderer.domElement);
   }
 
-  // get the vr user information
-  let vrFrameData = new VRFrameData();
-  this.vrDisplay.getFrameData(vrFrameData);
-
-  renderer.autoClear = false;
-  scene.matrixAutoUpdate = false;
-  camera.matrixAutoUpdate = false;
-  renderer.clear();
-
-  if (torus) {
-    torus.rotation.y += 0.01;
+  prepareLight() {
+    this.lights = {};
+    this.lights['ambient'] = new Three.AmbientLight(0xffffff, 0.5);
+    this.lights['directional'] = new Three.DirectionalLight(0xffffff, 1);
+    this.lights['directional'].position.set(10, 20, 20);
+    this.scene.add(this.lights['ambient']);
+    this.scene.add(this.lights['directional']);
   }
 
-  // only draw the first half
-  renderer.setViewport(0, 0, window.innerWidth * 0.5, window.innerHeight);
-  const lProjectionMatrix = vrFrameData.leftProjectionMatrix;
-  const lViewMatrix = vrFrameData.leftViewMatrix;
-  camera.projectionMatrix.fromArray(lProjectionMatrix);
-  scene.matrix.fromArray(lViewMatrix);
-  scene.updateMatrixWorld(true);
-  renderer.render(scene, camera);
+  async prepareGeometry() {
+    this.objects = {};
+    let loader = new Three.TextureLoader();
 
-  // clear depth buffer
-  renderer.clearDepth();
+    let map = await loader.load(require('./textures/earth.jpg'));
+    let normalMap = await loader.load(require('./textures/earth_normal.jpg'));
 
-  // only draw the second half
-  renderer.setViewport(window.innerWidth * 0.5, 0, window.innerWidth * 0.5, window.innerHeight);
-  const rProjectionMatrix = vrFrameData.rightProjectionMatrix;
-  const rViewMatrix = vrFrameData.rightViewMatrix;
-  camera.projectionMatrix.fromArray(rProjectionMatrix);
-  scene.matrix.fromArray(rViewMatrix);
-  scene.updateMatrixWorld(true);
-  renderer.render(scene, camera);
-  this.vrDisplay.requestAnimationFrame(render);
-  this.vrDisplay.submitFrame();
-};
+    this.objects['earth'] = new Three.Mesh(
+      new Three.SphereGeometry(2, 256, 256, 256),
+      new Three.MeshPhongMaterial({ map, normalMap, shininess: 0 })
+    );
+    this.objects['earth'].position.z = -10;
+    this.scene.add(this.objects['earth']);
+  }
 
-if (!navigator.getVRDisplays) {
-  alert('Your brower does not support WebVR :(');
-} else {
-  navigator.getVRDisplays().then(displays => {
-    // Filter down to devices that can present
-    displays = displays.filter(display => display.capabilities.canPresent);
+  partialRender(
+    projectionMatrix: number[],
+    viewMatrix: number[],
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  ) {
+    this.renderer.setViewport(x, y, width, height);
+    this.camera.projectionMatrix.fromArray(projectionMatrix);
+    this.scene.matrix.fromArray(viewMatrix);
+    this.scene.updateMatrixWorld(true);
+    this.renderer.render(this.scene, this.camera);
+  }
 
-    // if no devices available, quit.
-    if (displays.length == 0) {
-      alert('No devices available able to present.');
-      return;
+  update() {
+    this.objects['earth'].rotation.y += 0.01;
+  }
+
+  render() {
+    if (this.firstVRFrame) {
+      this.firstVRFrame = false;
+      return this.vrDisplay.requestAnimationFrame(this.render.bind(this));
     }
 
-    // store the first display we find
-    this.vrDisplay = displays[0];
-    this.vrDisplay.depthNear = NEAR;
-    this.vrDisplay.depthFar = FAR;
+    // get the vr user information
+    let vrFrameData = new VRFrameData();
+    this.vrDisplay.getFrameData(vrFrameData);
 
-    this.vrDisplay.requestPresent([{ source: renderer.domElement }])
-      .then(() => {
-        console.log('VR display initialized. Start rendering ...');
-        render();
-      })
-      .catch(e => alert('Failed to initialize VR display.' + e.printStack()));
-  });
+    this.renderer.autoClear = false;
+    this.scene.matrixAutoUpdate = false;
+    this.camera.matrixAutoUpdate = false;
+    this.renderer.clear();
+
+    this.update();
+
+    // only draw the first half
+    this.partialRender(
+      vrFrameData.leftProjectionMatrix,
+      vrFrameData.leftViewMatrix,
+      0,
+      0,
+      window.innerWidth * 0.5,
+      window.innerHeight
+    );
+
+    // clear depth buffer
+    this.renderer.clearDepth();
+
+    // only draw the second half
+    this.partialRender(
+      vrFrameData.rightProjectionMatrix,
+      vrFrameData.rightViewMatrix,
+      window.innerWidth * 0.5,
+      0,
+      window.innerWidth * 0.5,
+      window.innerHeight
+    );
+
+    this.vrDisplay.requestAnimationFrame(this.render.bind(this));
+    this.vrDisplay.submitFrame();
+  }
+
+  async initVR() {
+    if (!navigator.getVRDisplays) {
+      throw new Error('Your brower does not support WebVR :(');
+    }
+
+    navigator.getVRDisplays().then(async displays => {
+      // Filter down to devices that can present
+      displays = displays.filter(display => display.capabilities.canPresent);
+
+      // if no devices available, quit.
+      if (displays.length == 0) {
+        throw new Error('No devices available able to present.');
+      }
+
+      // store the first display we find
+      this.vrDisplay = displays[0];
+      this.vrDisplay.depthNear = NEAR;
+      this.vrDisplay.depthFar = FAR;
+
+      try {
+        await this.vrDisplay.requestPresent([{ source: this.renderer.domElement }]);
+      } catch(e) {
+        throw new Error('Failed to initialize VR display.');
+      }
+    });
+  }
 }
+
+let app: App = new App();
+
+(async () => {
+  try {
+    await app.initVR();
+    app.prepareLight();
+    await app.prepareGeometry();
+    app.render();
+  } catch (e) {
+    alert(e);
+  }
+})();
